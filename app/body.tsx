3f88e39a07235b6b2628cd187b794ee38271cf3d
cesarsan8a <cesarm.sanchezo@gmail.com>
2025-01-15 15:59:42 +1100

    "first commit"

---
+++ app/body.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Body() {
+    return(
+        <div id="pageContainer">
+
+        <div id="loadingMask"></div>
+
+        <div id="welcomeBannerContainer"></div>
+
+        <div id="profileImageContainer">
+            <div id="pinkCircle" className="circle"></div>
+            <img id="profilePic" className="circle" src="#" alt="Cee's profile picture" />
+            <div id="blueCircle" className="circle"></div>
+        </div>
+
+        <div id="infoContainer">
+            <div className="infoSection">
+                <h2 className="heading">Hi</h2>
+                <h3 className="subheading">I'm a web developer based in Melbourne, Australia</h3>
+                <p className="text">I have a passion for frontend development, and I love to build great code that makes <span id="nerdy">nerdy</span> cool stuff happen.</p>
+            </div>
+        </div>
+
+        <div id="infoContainer">
+            <div className="infoSection">
+                <h2 className="heading">What can I do?</h2>
+                <h3 className="subheading">I can design what you want</h3>
+                <p className="text">I enjoy figuring out how to bring ideas to life using code. I have experience developing with HTML, CSS, JavaScript and React. Check out my <a id="projectsLink" href="#">projects</a> to see examples of my work.</p>
+            </div>
+        </div>
+        </div>
+    );
+}
---
+++ app/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+    return(
+        <footer>
+            <img id="apolloContact" src="../images/apollo_phone.jpg" alt="Apollo in a hoodie holding a phone" />
+
+            <div id="contactText">
+                <h2 id="sayHello">Say hello</h2>
+                <p id="farewellMsg">Feel free to contact me at <a id="email" href="#">xceeochoax@gmail.com</a> or through any of my socials. Bye!</p>
+            </div>        
+        </footer>
+    );
+}
