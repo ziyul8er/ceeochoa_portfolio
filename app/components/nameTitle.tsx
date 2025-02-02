e1a447df0ac562174c3a0cc22e7d17d3b2c81c59
cesarsan8a <cesarm.sanchezo@gmail.com>
2025-02-02 20:18:18 +1100

    feat: add marquee

--- app/components/nameTitle.tsx
+++ app/components/nameTitle.tsx
@@ -1,71 +1,26 @@
 'use client';
-import React, {  useRef, useState } from "react";
+import React, { useState } from "react";
 import localFont from 'next/font/local';
 import { NextFont } from "next/dist/compiled/@next/font";
-import { motion } from 'motion/react';
+import { motion, useMotionValue, useTime, useTransform } from 'motion/react';
 
 import '../styles/nameTitle.css';
 import '../global.css';
 
-interface IFonts { 
-    [ index: number ]: NextFont;
-}
-
 // Fonts
-const defaultFont = localFont({ src: "../fonts/franchise-free-bold.ttf" });
-const engineerFont = localFont({ src: "../fonts/outward-block-webfont.woff2" });
-const hackerFont = localFont({ src: "../fonts/st-marie.woff2"});
-const devFont = localFont({ src: "../fonts/pixelated-elegance-regular-ovyAA.ttf"});
-
-const fontsArray: IFonts = [ defaultFont, engineerFont, hackerFont, devFont ];
+const boderFont: NextFont = localFont({ src: "../fonts/outward-borders-webfont.woff2" });
+const blockFont: NextFont = localFont({ src: "../fonts/outward-block-webfont.woff2" });
 
 export default function NameTitle() {
-
-    const [ displayFont, setDisplayFont ] = useState( fontsArray[1] );
-
-    function changeThemes(x: any) {
-        let chosenTheme = x.target.className;
-
-        switch (chosenTheme) {
-            case 'nt-nameTitle-engineer':
-                chosenTheme = engineerFont;
-                break;
-            case 'nt-nameTitle-dev':
-                chosenTheme = devFont;
-                break;
-            case 'nt-nameTitle-hacker':
-                chosenTheme = hackerFont;
-                break;                
-        }
-        setDisplayFont(chosenTheme);
-    }    
+    const time = useTime();
+    const x = useTransform(time, [0, 1000], [0, 100], {clamp: false});
 
     return(
-        <div className={ "nt-nameTitle " + displayFont.className }>
+        <div className="nt-nameTitle">
+            <span className={"nt-nameTitle-me " + boderFont.className}>CEE OCHOA</span>
             <motion.div 
-                className="nt-nameTitle-me"
-            >
-                <span>Cee Ochoa</span>
-            </motion.div>
-            <div className="u-themesWrapper">
-                <div className="nt-nameTitle-engineer"
-                    onClick={(e) => changeThemes(e)}
-                >
-                    ENGINEER
-                </div>
-
-                <div className="nt-nameTitle-dev"
-                    onClick={(e) => changeThemes(e)}
-                >
-                    WEB DEVELOPER
-                </div>
-
-                <div className="nt-nameTitle-hacker"
-                    onClick={(e) => changeThemes(e)}
-                >
-                    HACKER
-                </div>
-            </div>
+                style={{x: x}}
+            className={"nt-nameTitle-role " + blockFont.className}>·WEB DEVELOPER·</motion.div>
         </div>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Nav from "./components/navigation";
-import Footer from "./components/footer";
 import './global.css';
 
 const geistSans = Geist({
@@ -29,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Nav/>
         {children}
-        <Footer/>
       </body>
     </html>
   );
