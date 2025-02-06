18d60f1d2d89f40cc3cda6108f25af5f76eab414
cesarsan8a <cesarm.sanchezo@gmail.com>
2025-02-06 20:14:42 +1100

    feat: cheackpoint

--- app/components/marquee.tsx
+++ app/components/marquee.tsx
@@ -1,40 +1,76 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion, useAnimationFrame, useTime, useTransform } from 'motion/react';
 import { NextFont } from "next/dist/compiled/@next/font";
 import localFont from "next/font/local";
-import { motionValue } from "motion";
 
 const blockFont: NextFont = localFont({ src: "../fonts/outward-block-webfont.woff2" });
 
+interface IMarquee {
+    direction: boolean;
+    flexRow: boolean;
+}
 
-export default function Marquee() {
-    const ref = useRef<HTMLDivElement>(null)
-    const time = useTime();
-    const distanceTravelled = 1000;
+export default function Marquee( props: IMarquee) {
+    const [marqueeOffset, setMarqueeOffset] = useState<string | undefined>(undefined);
     
-    console.log(time.get());
-    useAnimationFrame((time) => {
-        if (!ref.current) return;
-
-        const x = (Math.sin(time / 9000)) * distanceTravelled;
-        ref.current.style.transform = `translateX(${x}px)`;
-    });
-
+    const ref = useRef<HTMLDivElement>(null);
+    const speed: number = 1000;
+    const distanceTravelled: number = 400;
     const web: string = 'WEB';
     const dev: string = 'DEVELOPER';
     const space: string = '·';
 
     let  marquee: string = space;
-
-    for (let i=0; i<10; i++) {
+    
+    for (let i = 0; i < 10; i++) {
         marquee = marquee + web + space + dev + space;
     }
 
+    const classes = `nt-nameTitle-marquee ${blockFont.className}`;
+    const hidden = props.flexRow === true ? " nt-nameTitle-marquee--hidden" : "";
+
+    function getMarqueeOffset(ref: HTMLDivElement) {
+        const marqueeStyles = getComputedStyle(ref);
+        const currentOffset = marqueeStyles.getPropertyValue('right');
+
+        const marqueeOffsetNumber = Number(currentOffset.slice(0, -2)) / 2;
+        let marqueeOffset: string;
+
+        if (typeof marqueeOffsetNumber !== "number") {
+            setMarqueeOffset(undefined);
+            return;
+        }
+
+        marqueeOffset = marqueeOffsetNumber.toString();
+
+        if(window.innerWidth < 640) {
+            setMarqueeOffset(undefined);
+            return;
+        }
+        console.log(marqueeOffset);
+        setMarqueeOffset(marqueeOffset);
+    }
+
+    useEffect(() => {
+        if(!ref.current) return;
+        getMarqueeOffset(ref.current);
+
+        window.addEventListener('resize', (screen) => {
+            if (!ref.current) return;
+            getMarqueeOffset(ref.current);
+        });        
+    }, [marqueeOffset]);
+
+    useAnimationFrame((t) => {
+        if (!ref.current) return;
+
+        const x = (props.direction === true ? 1 : -1) * (Math.sin(t / speed)) * distanceTravelled;
+        
+        ref.current.style.transform = `translateX(${x}px)`;
+    });
+
     return(            
-        <motion.div
-            ref={ref}
-            className={"nt-nameTitle-role " + blockFont.className}
-        >
+        <motion.div ref={ref} className={classes + hidden} style={{right: marqueeOffset}}>
             {marquee}
         </motion.div>    
     );
--- app/components/nameTitle.tsx
+++ app/components/nameTitle.tsx
@@ -1,22 +1,24 @@
 'use client';
-import React, { useEffect, useRef, useState } from "react";
+import React, { RefAttributes, useEffect, useRef, useState } from "react";
 import localFont from 'next/font/local';
 import { NextFont } from "next/dist/compiled/@next/font";
 import { nameSvgArray, surnameSvgArray } from "../variables/svgPaths";
+import Marquee from "./marquee";
 import '../styles/nameTitle.css';
 import '../global.css';
-import { motion, useAnimationFrame } from "motion/react";
 
 // Fonts
 const blockFont: NextFont = localFont({ src: "../fonts/outward-block-webfont.woff2" });
 
 export default function NameTitle() {
     const [letterOffset, setLetterOffset] = useState<string | undefined>(undefined);
+    const [flexRow, setFlexRow] = useState<boolean>(false);
+    const [marqueeOffset, setMarqueeOffset] = useState<string | undefined>(undefined);
 
-    const marqueeTopRef = useRef<HTMLDivElement>(null);
-    const marqueeBotRef = useRef<HTMLDivElement>(null);
     const nameRef = useRef<HTMLDivElement>(null);
     const surnameRef = useRef<HTMLDivElement>(null);
+    const marqueeTopRef = useRef<HTMLDivElement>(null);
+    const marqueeBotRef = useRef<HTMLDivElement>(null);
 
     function getLetterOffset(ref:HTMLDivElement) {
         const nameStyles = getComputedStyle(ref);
@@ -30,61 +32,63 @@ export default function NameTitle() {
         setLetterOffset(nameWidth);
     }
 
+    function getMarqueeOffset(ref: HTMLDivElement) {
+        const marqueeStyles = getComputedStyle(ref);
+        const currentOffset = marqueeStyles.getPropertyValue('right');
+
+        const marqueeOffsetNumber = Number(currentOffset.slice(0, -2)) / 2;
+        let marqueeOffset: string;
+
+        if (typeof marqueeOffsetNumber !== "number") {
+            setMarqueeOffset(undefined);
+            return;
+        }
+
+        marqueeOffset = marqueeOffsetNumber.toString();
+
+        if(window.innerWidth < 640) {
+            setMarqueeOffset(undefined);
+            return;
+        }
+        console.log(marqueeOffset);
+        setMarqueeOffset(marqueeOffset);
+    }
+
+    function flexDirection() {
+        if(window.innerWidth < 640) {
+            setFlexRow(false);
+            return;
+        }
+        setFlexRow(true);
+    }
+
     useEffect(() => {
+        flexDirection();
+
         if (!nameRef.current) return;
         getLetterOffset(nameRef.current);
 
         window.addEventListener('resize', (screen) => {
             if (!nameRef.current) return;
             getLetterOffset(nameRef.current);
+            flexDirection();
         });
-    }, [screen, letterOffset]);
+    }, [screen, letterOffset, flexRow, marqueeOffset]);
 
-    //animation
     const surnameStyles = letterOffset ? {left: letterOffset} : {};
-    const speed: number = 4000;
-    const distanceTravelled: number = 400;
-    const web: string = 'WEB';
-    const dev: string = 'DEVELOPER';
-    const space: string = '·';
-
-    let  marquee: string = space;
-
-    for (let i = 0; i < 10; i++) {
-        marquee = marquee + web + space + dev + space;
-    }
-
-    useAnimationFrame((t) => {
-        if (!marqueeTopRef.current) return;
-        if (!marqueeBotRef.current) return;
-
-        const xTop = (Math.sin(t / speed)) * distanceTravelled;
-        const xBottom = -(Math.sin(t / speed)) * distanceTravelled;
-
-        marqueeTopRef.current.style.transform = `translateX(${xTop}px)`;
-        marqueeBotRef.current.style.transform = `translateX(${xBottom}px)`;
-
-    });
 
     return(
         <div className="nt-nameTitle">
             <div ref={nameRef} className="nt-nameTitle-name">
                 {nameSvgArray.map(el => el)}
-                <motion.div ref={marqueeTopRef}
-                    className={"nt-nameTitle-marquee " + blockFont.className}
-                >
-                    {marquee}
-                </motion.div>
+                <Marquee direction={true} flexRow={flexRow} />
             </div>
 
             <div ref={surnameRef} className="nt-nameTitle-surname" style={{...surnameStyles}}>
                 {surnameSvgArray.map(el => el)}
-                <motion.div ref={marqueeBotRef}
-                    className={"nt-nameTitle-marquee nt-nameTitle-marquee--row " + blockFont.className}
-                >
-                    {marquee}
-                </motion.div>
+                <Marquee direction={false} flexRow={flexRow} />
             </div>
+            <Marquee direction={false} flexRow={!flexRow} />
         </div>
     );
 }
