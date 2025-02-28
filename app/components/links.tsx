7a9d325d24e0d02b59480b07fada7ffac27deef7
cesarsan8a <cesarm.sanchezo@gmail.com>
2025-02-28 11:52:19 +1100

    feat: move links css to its own file

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -1,26 +1,27 @@
-"use client";
-import { motion } from "motion/react";
-import { mailIcon, linkedInIcon } from "../variables/svgPaths";
+'use client';
+import { motion } from 'motion/react';
+import { mailIcon, linkedInIcon } from '../variables/svgPaths';
+import '../styles/links.css';
 
 export default function Links() {
-  return (
-    <div className="u-linksWrapper">
-      <motion.a
-        className="nt-mailLink"
-        href="mailto:xceeochoax@gmail.com"
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.8 }}
-      >
-        {mailIcon}
-      </motion.a>
-      <motion.a
-        className="nt-mailLink"
-        href="http://www.linkedin.com/in/ceeochoa"
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.8 }}
-      >
-        {linkedInIcon}
-      </motion.a>
-    </div>
-  );
+	return (
+		<div className="u-linksWrapper">
+			<motion.a
+				className="nt-mailLink"
+				href="mailto:xceeochoax@gmail.com"
+				whileHover={{ scale: 1.2 }}
+				whileTap={{ scale: 0.8 }}
+			>
+				{mailIcon}
+			</motion.a>
+			<motion.a
+				className="nt-mailLink"
+				href="http://www.linkedin.com/in/ceeochoa"
+				whileHover={{ scale: 1.2 }}
+				whileTap={{ scale: 0.8 }}
+			>
+				{linkedInIcon}
+			</motion.a>
+		</div>
+	);
 }
