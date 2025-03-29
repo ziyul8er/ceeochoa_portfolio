266098c2c2714eb96ff01463627f3d649136a21e
cesarsan8a <cesarm.sanchezo@gmail.com>
2025-03-29 16:51:17 +1100

    feat: mobile view of main title

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { DeveloperName } from './components/devName';
 import Links from './components/links';
+import localFont from 'next/font/local';
+import { NextFont } from 'next/dist/compiled/@next/font';
+
+const ibmFont: NextFont = localFont({
+	src: 'fonts/IBMPlexMono-Regular.ttf'
+});
 
 export default function Home() {
 	return (
 		<>
 			<div className="main-title">
 				<DeveloperName />
-				<Links />
+				<div className={ibmFont.className + ' intro'}>
+					Hey I’m Cee. I am a Web Developer, scientist at heart and
+					passionate about gradening. Currently living and working in{' '}
+					<span>Caracas</span> Naarm (Melborune).
+				</div>
+				{/* <Links /> */}
 			</div>
 		</>
 	);
