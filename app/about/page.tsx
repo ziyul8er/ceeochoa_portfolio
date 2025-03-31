17d41079a763b692e126997832f1e72ed7ee6f4b
cesarsan8a <cesarm.sanchezo@gmail.com>
2025-03-31 16:51:38 +1100

    refactor: fix characters viewport

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,23 +11,25 @@ import createSVGElement, {
 
 export default function About() {
 	const aLetter = createSVGElement(aChunkLetter, 833);
-	const bLetter = createSVGElement(bChunkLetter, 833);
-	const oLetter = createSVGElement(oChunkLetter, 833);
-	const uLetter = createSVGElement(uChunkLetter, 833);
-	const tLetter = createSVGElement(tChunkLetter, 833);
+	const bLetter = createSVGElement(bChunkLetter, 697);
+	const oLetter = createSVGElement(oChunkLetter, 750);
+	const uLetter = createSVGElement(uChunkLetter, 747);
+	const tLetter = createSVGElement(tChunkLetter, 711);
 
 	return (
-		// deberia poner una flecha para ir para atras
-		<div className="window">
-			<div className="main-heading">
-				<div className="main-heading__about">
-					{aLetter}
-					{bLetter}
-					{oLetter}
-					{uLetter}
-					{tLetter}
+		<>
+			{/* deberia poner una flecha para ir para atras */}
+			<div className="window">
+				<div className="main-heading">
+					<div className="main-heading__about">
+						{aLetter}
+						{bLetter}
+						{oLetter}
+						{uLetter}
+						{tLetter}
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
