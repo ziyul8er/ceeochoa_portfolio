c93eef8e01f7bf39a01438ac135b2dce982f0d75
cesarsan8a <cesarm.sanchezo@gmail.com>
2025-04-11 16:50:01 +1000

    feat: add dev pic to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,6 +9,7 @@ import createSVGElement, {
 	tChunkLetter
 } from '../variables/svgPaths';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function About() {
 	const aLetter = createSVGElement(aChunkLetter, 833);
@@ -33,15 +34,28 @@ export default function About() {
 						Cee is a web developer, engineer, mad scientist and
 						music enjoyer/explorer
 					</p>
+					<div className="wall">
+						<p className="wall__top-div">
+							{"<div className='404_wall'>"}
+						</p>
+						<Image
+							className="wall__dev-pic"
+							src="/media/dev_pic.png"
+							width={84}
+							height={200}
+							alt={"Developer's photo leaning on a wall"}
+						></Image>
+						<p className="wall__bot-div">{'</div>'}</p>
+					</div>
+
 					<p>Born and raised in the Caribbean</p>
 					<p>
 						Based: <s>Caracas</s> <s>Oxford</s> Naarm/Melbourne
 					</p>
-					<p>Let's chat!</p>
+					<p>Let&#39;s chat!</p>
 					<Link href="mailto:xceeochoax@gmail.com">
 						xceeochoax@gmail.com
 					</Link>
-					<p>Brunswick - VIC 3056 - Australia</p>
 				</div>
 			</div>
 		</>
